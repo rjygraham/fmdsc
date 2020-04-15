ac2b75e44f8a657e6ee3da74479020c955d26d5a
Ryan Graham <rygraham@microsoft.com>
2020-04-15 16:57:25 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,87 @@
+# File Metadata Sidecar (fmdsc)
+
+The File Metadata Sidecar is useful in situations where file attributes need to be maintained when copying files across storage systems that do not persist (or support) file attributes.
+
+File Metadata Sidecar works by scanning the supplied path for all content files and creating a .meta file alongside the content file. Once the content files have reached their destination or on a storage system that supports file attributes, run fmdsc again to restore the file attributes from the .meta file and optionally delete the .meta file.
+
+## Usage
+
+To use fmdsc, you need to specify either the `create` or `restore` verbs depending on which operation you're trying to run.
+
+````
+C:>fmdsc help
+fmdsc 0.1.0
+Copyright (C) 2020 fmdsc
+
+create     Create file attributes metadata files.
+
+restore    Restore file attributes.
+
+help       Display more information on a specific command.
+
+version    Display version information.
+````
+
+### Create
+
+To create the metadata files, you'll execute `fmdsc create` and specify the path with the `-p` option.
+
+````
+C:>fmdsc create --help
+fmdsc 0.1.0
+Copyright (C) 2020 fmdsc
+
+-p, --path    Required. Set the input folder to scan.
+
+--help        Display this help screen.
+
+--version     Display version information.
+````
+
+For example, to create metadata files for all files (recursively) beneath `C:\example` you would execute the following command:
+
+````
+C:>fmdsc create -p 'C:\example'
+````
+
+### Restore
+
+To restore file attributes, you'll execute `fmdsc restore` and specify the path with the `-p` option. You can also optionally provide the `-d` option to delete the metadata files.
+
+````
+C:>fmdsc restore help
+fmdsc 0.1.0
+Copyright (C) 2020 fmdsc
+
+ERROR(S):
+Required option 'p, path' is missing.
+
+-p, --path      Required. Set the input folder to scan.
+
+-d, --delete    Delete the source metadata file.
+
+--help          Display this help screen.
+
+--version       Display version information.
+````
+
+For example, to restore the file attributes for all files (recursively) beneath `C:\example` you would execute the following command:
+
+````
+C:>fmdsc restore -p 'C:\example'
+````
+
+And if you wanted to delete the source metadata files, you would exeucte the following command:
+
+````
+C:>fmdsc restore -d -p 'C:\example'
+````
+
+### Logging
+
+fmdsc only logs to console output. If you need to record progress in a log file, pipe output to your file of choice. For example:
+
+````
+C:>fmdsc create -p 'C:\example' > 'C:\logs\fmdsc-create.log'
+C:>fmdsc restore -p 'C:\example' > 'C:\logs\fmdsc-restore.log'
+````
---
+++ src/Rgom.FileMetadata.Sidecar/Program.cs
@@ -0,0 +1,149 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Rgom.FileMetadata.Sidecar
+{
+	class Program
+	{
+		private static HashSet<string> files;
+		private static int currentCounter = 0;
+
+		[Verb("create", HelpText = "Create file attributes metadata files.")]
+		class ExtractOptions
+		{
+			[Option('p', "path", Required = true, HelpText = "Set the input folder to scan.")]
+			public string Path { get; set; }
+		}
+
+		[Verb("restore", HelpText = "Restore file attributes.")]
+		class RestoreOptions
+		{
+			[Option('p', "path", Required = true, HelpText = "Set the input folder to scan.")]
+			public string Path { get; set; }
+
+			[Option('d', "delete", Required = false, HelpText = "Delete the source metadata file.")]
+			public bool Delete { get; set; }
+		}
+
+		struct Metadata
+		{
+			[JsonPropertyName("c")]
+			public DateTime CreationTimeUtc { get; set; }
+			[JsonPropertyName("w")]
+			public DateTime LastWriteTimeUtc { get; set; }
+			[JsonPropertyName("a")]
+			public DateTime LastAccessTimeUtc { get; set; }
+		}
+
+		static int Main(string[] args)
+		{
+			var result = Parser.Default.ParseArguments<ExtractOptions, RestoreOptions>(args)
+			.MapResult(
+			  (ExtractOptions opts) => RunExtractAndReturnExitCode(opts),
+			  (RestoreOptions opts) => RunRestoreAndReturnExitCode(opts),
+			  errs => 1);
+
+			return result;
+		}
+
+		private static int RunExtractAndReturnExitCode(ExtractOptions options)
+		{
+			if (!ValidateInputPath(options.Path))
+			{
+				return -1;
+			}
+
+			Console.WriteLine($"Recursivle processing all files in: {options.Path}");
+
+			files = new HashSet<string>(Directory.GetFiles(options.Path, "*", SearchOption.AllDirectories));
+
+			Parallel.ForEach(files, file =>
+			{
+				try
+				{
+					var fileInfo = new FileInfo(file);
+
+					Console.WriteLine($"Processing: {fileInfo.FullName}");
+
+					var metadata = new Metadata
+					{
+						CreationTimeUtc = fileInfo.CreationTimeUtc,
+						LastWriteTimeUtc = fileInfo.LastWriteTimeUtc,
+						LastAccessTimeUtc = fileInfo.LastAccessTimeUtc
+					};
+
+					File.WriteAllText($"{fileInfo.FullName}.meta", JsonSerializer.Serialize(metadata));
+
+					Interlocked.Increment(ref currentCounter);
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine();
+					Console.WriteLine($"ERROR: {file}, MESSAGE: {ex.Message}");
+					Console.WriteLine();
+				}
+			});
+
+			Console.WriteLine($"{currentCounter} / {files.Count} complete!");
+
+			return 0;
+		}
+
+		private static int RunRestoreAndReturnExitCode(RestoreOptions options)
+		{
+			if (!ValidateInputPath(options.Path))
+			{
+				return -1;
+			}
+
+			Console.WriteLine($"Recursivle processing all files in: {options.Path}");
+
+			files = new HashSet<string>(Directory.GetFiles(options.Path, "*.meta", SearchOption.AllDirectories));
+
+			Parallel.ForEach(files, file  =>
+			{
+				try
+				{
+					var fileInfo = new FileInfo(file);
+
+					Console.WriteLine($"Processing: {fileInfo.FullName}");
+
+					var metadata = JsonSerializer.Deserialize<Metadata>(File.ReadAllText(fileInfo.FullName));
+					var targetFileName = fileInfo.FullName.Remove(fileInfo.FullName.Length - 5);
+
+					File.SetCreationTimeUtc(targetFileName, metadata.CreationTimeUtc);
+					File.SetLastWriteTimeUtc(targetFileName, metadata.LastWriteTimeUtc);
+					File.SetLastAccessTimeUtc(targetFileName, metadata.LastAccessTimeUtc);
+
+					if (options.Delete)
+					{
+						File.Delete(fileInfo.FullName);
+					}
+
+					Interlocked.Increment(ref currentCounter);
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine();
+					Console.WriteLine($"ERROR: {file}, MESSAGE: {ex.Message}");
+					Console.WriteLine();
+				}
+			});
+
+			Console.WriteLine($"{currentCounter} / {files.Count} complete!");
+
+			return 0;
+		}
+
+		private static bool ValidateInputPath(string path)
+		{
+			return Directory.Exists(path);
+		}
+	}
+}
